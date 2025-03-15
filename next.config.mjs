3b3d2d71bfe84b5083f9daa41036a4c3037bd0a6
Farsin Konnadan <farsin495@gmail.com>
2025-03-15 14:20:14 +0530

    First Commit

---
+++ next.config.mjs
@@ -0,0 +1,8 @@
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  reactStrictMode: true,
+  images: {
+    domains: ["res.cloudinary.com", "via.placeholder.com"],
+  },
+}
+export default nextConfig;
